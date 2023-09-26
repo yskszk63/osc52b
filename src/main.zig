02a6a54d83dbc047046a1f219e36a3262042921a
yusuke suzuki <yskszk63@gmail.com>
2023-09-26 11:14:28 +0900

    SSH_TTY (#1)

--- src/main.zig
+++ src/main.zig
@@ -8,21 +8,30 @@ fn usage() noreturn {
 }
 
 pub fn main() anyerror!void {
+    const alloc = std.heap.page_allocator;
+
+    const ssh_tty_or_null = std.process.getEnvVarOwned(alloc, "SSH_TTY") catch null;
+
     const stdout = std.io.getStdOut().writer();
 
-    const alloc = std.heap.page_allocator;
     var args = std.process.args();
     if (!args.skip()) {
         usage();
     }
-    const arg: []u8 = try args.next(alloc) orelse {
+    const arg = args.next() orelse {
         usage();
     };
-    defer alloc.free(arg);
 
     var buf: [0x1000]u8 = undefined;
     const b = std.base64.standard.Encoder.encode(&buf, arg);
 
     try stdout.print("🌏 {s}\n", .{arg});
-    try stdout.print("\x1b]52;c;{s}\x07", .{b});
+    if (ssh_tty_or_null) |tty| {
+        const fp = try std.fs.openFileAbsolute(tty, .{ .mode = std.fs.File.OpenMode.write_only });
+        defer fp.close();
+
+        try fp.writer().print("\x1b]52;c;{s}\x07", .{b});
+    } else {
+        try stdout.print("\x1b]52;c;{s}\x07", .{b});
+    }
 }
